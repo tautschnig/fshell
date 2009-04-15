54582173dd5af645f67b750d54801f4ea8175ef4
tautschnig <tautschnig@740c81f5-4728-4b1f-bc77-cf82a461b733>
2009-04-15 10:05:36 +0000

    Updated script to the proper defaults in fshell2
    git-svn-id: https://svn.forsyte.informatik.tu-darmstadt.de/svn/fshell/trunk@18 740c81f5-4728-4b1f-bc77-cf82a461b733

--- config/create_file.sh
+++ config/create_file.sh
@@ -40,22 +40,20 @@ case "$fname" in
 /* vi: set ts=4 sw=4 noexpandtab: */
 
 /*******************************************************************************
- * PACKAGE, PACKAGE_DESCRIPTION
- * Copyright (C) YEAR  AUTHORS
+ * FShell 2
+ * Copyright 2009 Michael Tautschnig, tautschnig@forsyte.de
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
 
 /*! \file $fname
@@ -68,8 +66,8 @@ case "$fname" in
 
 
 #include <diagnostics/unittest.hpp>
-#include <SOURCE/config/config.hpp>
-#include <SOURCE/config/annotations.hpp>
+#include <fshell2/config/config.hpp>
+#include <fshell2/config/annotations.hpp>
 
 #include <`echo $fname | sed 's/.t.cpp$/.hpp/'`>
 
@@ -114,22 +112,20 @@ EOF
 /* vi: set ts=4 sw=4 noexpandtab: */
 
 /*******************************************************************************
- * PACKAGE, PACKAGE_DESCRIPTION
- * Copyright (C) YEAR  AUTHORS
+ * FShell 2
+ * Copyright 2009 Michael Tautschnig, tautschnig@forsyte.de
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
 
 #ifndef $pattern
@@ -143,7 +139,7 @@ EOF
  * \date   `date` 
 */
 
-#include <SOURCE/config/config.hpp>
+#include <fshell2/config/config.hpp>
 
 $begin_ns
 /*! \brief TODO
@@ -176,22 +172,20 @@ EOF
 /* vi: set ts=4 sw=4 noexpandtab: */
 
 /*******************************************************************************
- * PACKAGE, PACKAGE_DESCRIPTION
- * Copyright (C) YEAR  AUTHORS
+ * FShell 2
+ * Copyright 2009 Michael Tautschnig, tautschnig@forsyte.de
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *******************************************************************************/
 
 /*! \file $fname
