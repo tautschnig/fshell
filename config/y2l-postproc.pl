07f1accdcf3b415c093f5e4480f337a43e97a8be
tautschnig <tautschnig@740c81f5-4728-4b1f-bc77-cf82a461b733>
2009-04-15 11:52:08 +0000

    Added y2l to auto-build help from yacc files
    - y2l-postproc.pl is a home-grown script to prepare the proper output
    
    git-svn-id: https://svn.forsyte.informatik.tu-darmstadt.de/svn/fshell/trunk@22 740c81f5-4728-4b1f-bc77-cf82a461b733

---
+++ config/y2l-postproc.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $last_tok = "";
+
+print "#define HELP \\\n";
+
+while (<>) {
+  my $line = $_;
+  $line =~ s/(\s+|^)([a-zA-Z_]+)(\s+|$)/$1<$2>$3/g;
+  $line =~ s/"([^"]+)"/`$1'/g;
+  ($line =~ /^(<[a-zA-Z_]+>\s+::)(=)/) or die "$line was unexpected\n";
+  if ($1 eq $last_tok) {
+    my $blanks = $1;
+    $blanks =~ s/./ /g;
+    $line =~ s/$last_tok=/$blanks|/;
+  } else {
+    $last_tok = $1;
+  }
+
+  $line =~ s/^/"/;
+  $line =~ s/$/" << ::std::endl << \\/;
+  print $line;
+}
+
+print "\"\"\n";
+
