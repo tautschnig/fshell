d6783a00e9af047c52b004a4dac8f9d5a16e56ae
tautschnig <tautschnig@740c81f5-4728-4b1f-bc77-cf82a461b733>
2009-04-21 21:41:44 +0000

    Added blank class FQL_Node_Factory
    - This will be a template class that is able to build any FQL_Node object and
      retains its ownership
    
    git-svn-id: https://svn.forsyte.informatik.tu-darmstadt.de/svn/fshell/trunk@66 740c81f5-4728-4b1f-bc77-cf82a461b733

---
+++ fshell2/fql/ast/fql_node_factory.hpp
@@ -0,0 +1,60 @@
+/* -*- Mode: C++; tab-width: 4 -*- */
+/* vi: set ts=4 sw=4 noexpandtab: */
+
+/*******************************************************************************
+ * FShell 2
+ * Copyright 2009 Michael Tautschnig, tautschnig@forsyte.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+#ifndef FSHELL2__FQL__AST__FQL_NODE_FACTORY_HPP
+#define FSHELL2__FQL__AST__FQL_NODE_FACTORY_HPP
+
+/*! \file fshell2/fql/ast/fql_node_factory.hpp
+ * \brief TODO
+ *
+ * $Id$
+ * \author Michael Tautschnig <tautschnig@forsyte.de>
+ * \date   Mon Apr 20 23:54:15 CEST 2009 
+*/
+
+#include <fshell2/config/config.hpp>
+
+FSHELL2_NAMESPACE_BEGIN;
+FSHELL2_FQL_NAMESPACE_BEGIN;
+
+/*! \brief TODO
+*/
+class FQL_Node_Factory
+{
+	/*! \copydoc doc_self
+	*/
+	typedef FQL_Node_Factory Self;
+
+	public:
+
+	private:
+	/*! \copydoc copy_constructor
+	*/
+	FQL_Node_Factory( Self const& rhs );
+
+	/*! \copydoc assignment_op
+	 */
+	Self& operator=( Self const& rhs );
+};
+
+FSHELL2_FQL_NAMESPACE_END;
+FSHELL2_NAMESPACE_END;
+
+#endif /* FSHELL2__FQL__AST__FQL_NODE_FACTORY_HPP */
