2293086c9840ed66550985354d674298f5f09026
tautschnig <tautschnig@740c81f5-4728-4b1f-bc77-cf82a461b733>
2009-04-21 21:41:32 +0000

    Added blank FQL_AST_Printer class
    - + no-op test suite
    
    git-svn-id: https://svn.forsyte.informatik.tu-darmstadt.de/svn/fshell/trunk@65 740c81f5-4728-4b1f-bc77-cf82a461b733

---
+++ fshell2/fql/ast/fql_ast_printer.t.cpp
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 4 -*- */
+/* vi: set ts=4 sw=4 noexpandtab: */
+
+/*******************************************************************************
+ * FShell 2
+ * Copyright 2009 Michael Tautschnig, tautschnig@forsyte.de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+/*! \file fshell2/fql/ast/fql_ast_printer.t.cpp
+ * \brief TODO
+ *
+ * $Id$
+ * \author Michael Tautschnig <tautschnig@forsyte.de>
+ * \date   Mon Apr 20 23:54:01 CEST 2009 
+*/
+
+
+#include <diagnostics/unittest.hpp>
+#include <fshell2/config/config.hpp>
+#include <fshell2/config/annotations.hpp>
+
+#include <fshell2/fql/ast/fql_ast_printer.hpp>
+
+#define TEST_COMPONENT_NAME FQL_AST_Printer
+#define TEST_COMPONENT_NAMESPACE fshell2::fql
+
+FSHELL2_NAMESPACE_BEGIN;
+FSHELL2_FQL_NAMESPACE_BEGIN;
+
+/** @cond */
+TEST_NAMESPACE_BEGIN;
+TEST_COMPONENT_TEST_NAMESPACE_BEGIN;
+/** @endcond */
+using namespace ::diagnostics::unittest;
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * @test A test of FQL_AST_Printer
+ *
+ */
+void test( Test_Data & data )
+{
+}
+
+/** @cond */
+TEST_COMPONENT_TEST_NAMESPACE_END;
+TEST_NAMESPACE_END;
+/** @endcond */
+
+FSHELL2_FQL_NAMESPACE_END;
+FSHELL2_NAMESPACE_END;
+
+TEST_SUITE_BEGIN;
+TEST_NORMAL_CASE( &test, LEVEL_PROD );
+TEST_SUITE_END;
+
+STREAM_TEST_SYSTEM_MAIN;
