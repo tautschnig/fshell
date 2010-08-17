42009f3c23f2f202ff521a1d3d06c0b74ac02d7f
tautschnig <tautschnig@740c81f5-4728-4b1f-bc77-cf82a461b733>
2010-08-17 23:54:54 +0000

    Completely new benchmarking setup
    - added benchmark-* and profile targets to top makefile
    - use of benchmarks is described in benchmarks/README
    
    git-svn-id: https://svn.forsyte.informatik.tu-darmstadt.de/svn/fshell/trunk@259 740c81f5-4728-4b1f-bc77-cf82a461b733

---
+++ benchmarks/scripts/run_bench.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$debug" ] ; then
+  debug=0
+elif [ $debug -gt 0 ] ; then
+  set -x
+fi
+
+[ -n "$fshell" ] 
+[ -x "$fshell" ]
+[ -n "$logf" ]
+  
+benchmarks=$@
+results=`mktemp`
+trap 'rm -f $results' EXIT
+
+for b in $benchmarks ; do
+  unset source extra_params prepare query tg unsat tc cputime
+  . $b
+  make -C code $source.prep
+  rm -f .fshell_history
+  echo "Running benchmark $b"
+  if [ $debug -eq 1 ] ; then
+    echo -e "$prepare\n$query\nquit\n" | $fshell $params $extra_params
+  elif [ $debug -eq 2 ] ; then
+    echo -e "$prepare\n$query\nquit\n"
+    gdb --args $fshell $params $extra_params
+  else
+    ( time echo -e "$prepare\n$query\nquit\n" | $fshell $params $extra_params ) > $results 2>&1
+    mtg=`grep "Possibly feasible test goals:" $results | sed 's/.*Possibly feasible test goals: //'`
+    munsat=`grep "Test goals not fulfilled:" $results | sed 's/.*Test goals not fulfilled: //'`
+    mtc=`grep "Test cases:" $results | sed 's/.*Test cases: //'`
+    mcputime=`grep "Query CPU time:" $results | sed 's/.*Query CPU time: //'`
+    if [ -z "$mtg" -o -z "$munsat" -o -z "$mtc" -o -z "$mcputime" ] ; then
+      cp $results failure.log
+      exit 1
+    fi
+    echo "$b #tg=$mtg($tg) #iftg=$munsat($unsat) #tc=$mtc($tc) ${mcputime}($cputime)" >> $logf
+  fi
+done
+
