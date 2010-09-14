8e9227baa25017609f5285786683c58cb975cdbb
tautschnig <tautschnig@740c81f5-4728-4b1f-bc77-cf82a461b733>
2010-09-14 11:46:38 +0000

    Added further config flags mempeak, exec_ts, gcov, gcov_opts
    - Also add peak memory usage to benchmark logs
    - (Optionally) execute generated test suite using test environment builder
    - (Optionally) check statement or branch coverage (using gcov)
    - Enabled and working on benchmark suite "BASIC"
    
    git-svn-id: https://svn.forsyte.informatik.tu-darmstadt.de/svn/fshell/trunk@321 740c81f5-4728-4b1f-bc77-cf82a461b733

--- benchmarks/scripts/run_bench.sh
+++ benchmarks/scripts/run_bench.sh
@@ -10,21 +10,32 @@ fi
 
 [ -n "$fshell" ] 
 [ -x "$fshell" ]
+[ -n "$testenv" ] 
+[ -x "$testenv" ]
 [ -n "$logf" ]
   
 benchmarks=$@
 if which mktemp > /dev/null 2>&1 ; then
   results=`mktemp`
+  testsuite=`mktemp`
 else
   results="$logf.tmplog"
+  testsuite="$logf.tmpts"
 fi
-trap 'rm -f $results' EXIT
+trap 'rm -f $results $testsuite' EXIT
 
 for b in $benchmarks ; do
-  unset source extra_params prepare query tg unsat tc cputime
+  unset source extra_params prepare query tg unsat tc cputime mempeak exec_ts gcov gcov_opts
   . $b
   make -C code $source.prep
   rm -f .fshell_history
+  rm -f $testsuite
+  if [ x$exec_ts = xyes -o x$gcov = xyes ] ; then
+    exec_ts="yes"
+    extra_params="$extra_params --outfile $testsuite"
+  else
+    extra_params="$extra_params --outfile /dev/null"
+  fi
   echo "Running benchmark $b"
   if [ $debug -eq 1 ] ; then
     echo -e "$prepare\n$query\nquit\n" | $fshell $params $extra_params
@@ -37,11 +48,30 @@ for b in $benchmarks ; do
     munsat=`grep "Test goals not fulfilled:" $results | sed 's/.*Test goals not fulfilled: //'`
     mtc=`grep "Test cases:" $results | sed 's/.*Test cases: //'`
     mcputime=`grep "Query CPU time:" $results | sed 's/.*Query CPU time: //'`
-    if [ -z "$mtg" -o -z "$munsat" -o -z "$mtc" -o -z "$mcputime" ] ; then
+    mmempeak=`grep "Peak memory usage:" $results | sed 's/.*Peak memory usage: //'`
+    if [ -z "$mtg" -o -z "$munsat" -o -z "$mtc" -o -z "$mcputime" -o -z "$mmempeak" ] ; then
       cp $results failure.log
       exit 1
     fi
-    echo "$b #tg=$mtg($tg) #iftg=$munsat($unsat) #tc=$mtc($tc) ${mcputime}($cputime)" >> $logf
+    echo "$b #tg=$mtg($tg) #iftg=$munsat($unsat) #tc=$mtc($tc) ${mcputime}($cputime) ${mmempeak}($mempeak)" >> $logf
+    if [ x$exec_ts = xyes ] ; then
+      echo "Trying to execute generated test suite"
+      $testenv < $testsuite
+      [ -f tester.mk ]
+      make -f tester.mk clean > /dev/null
+      if [ x$gcov = xyes ] ; then
+        make -f tester.mk BUILD_FLAGS="-g -fprofile-arcs -ftest-coverage" > /dev/null
+        for f in *.mod.gcno ; do
+          gcov $gcov_opts "`echo $f | sed 's/mod.gcno/mod.c/'`"
+        done
+      else
+        make -f tester.mk BUILD_FLAGS="-g" > /dev/null
+      fi
+      make -f tester.mk clean > /dev/null
+      rm tester.mk tester.c
+      rm -f *.gcov *.gcda *.gcno
+      rm -rf tester.dSYM
+    fi
   fi
 done
 
